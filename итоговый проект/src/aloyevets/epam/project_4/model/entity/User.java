45b0c9bf06d434992c4ea524f2d84a466abce5ca
nikita9200 <139256355+nikita9200@users.noreply.github.com>
2023-08-15 20:32:13 +0300

    Add files via upload

---
+++ итоговый проект/src/aloyevets/epam/project_4/model/entity/User.java
@@ -0,0 +1,67 @@
+package aloyevets.epam.project_4.model.entity;
+
+public class User {
+	private int id;
+	private String email;
+	private String password;
+	private String name;
+	private String surname;
+	private String phone;
+	private boolean isActivated;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isActivated() {
+		return isActivated;
+	}
+
+	public void setActivated(boolean isActivated) {
+		this.isActivated = isActivated;
+	}
+}
